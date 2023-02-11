809138927f33e8ebde106c98111aa4ff53a3af0d
Luwam <luwamtmariam@gmail.com>
2023-02-11 01:23:19 -0600

    fixed minor errors

--- src/main/java/mscs/hms/repository/CompanyRepository.java
+++ src/main/java/mscs/hms/repository/CompanyRepository.java
@@ -7,7 +7,7 @@ import mscs.hms.model.Company;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-    @Query("select c from Company c, LegalEntity l , User u where c.legalEntityName = l.legalEntityName or l.systemUser.username = :searchString  or c.phoneNumber =:searchString or c.legalEntityName = :searchString or " +
-            "c.companyName = :searchString or l.systemUser.username = :searchString")
+    @Query("select c from Company c, LegalEntity l , User u where c.legalEntityName = l.legalEntityName and l.systemUser.username = :searchString  or c.phoneNumber =:searchString or c.legalEntityName = :searchString or " +
+            "c.companyName = :searchString")
     Page<Company> searchCompany(String searchString, PageRequest pageRequest);
 }
--- src/main/java/mscs/hms/repository/InquiryRepository.java
+++ src/main/java/mscs/hms/repository/InquiryRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
-    @Query("select i from Inquiry i where i.title = :searchString or i.property.name = :searchString or i.inquiryDetails =:searchString")
+    @Query("select i from Inquiry i where i.title = :searchString or i.inquiryDetails =:searchString or i.tenant.name =:searchString or i.property.name =:searchString or i.landlord.name =:searchString")
     Page<Inquiry> searchInquiries(String searchString, PageRequest pageRequest);
 
     Page<Inquiry> findByInquiryDate(LocalDate date, PageRequest pageRequest);
