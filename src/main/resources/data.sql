4944a127fe99bd3acd579963483c73d3921e0977
Luwam <luwamtmariam@gmail.com>
2023-02-11 01:35:00 -0600

    fixed minor errors

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,13 +28,13 @@ VALUES ('renter', 'renter@miu.edu', 'Renter', 'Renter','$2a$10$jcatEpTX1.L3Gq3KI
 -- User_Role
 
 INSERT INTO user_role (user_id, role_id)
-values ((SELECT user_id from users where user_id = 1), (SELECT role_id from roles where role_id = 1));
+values ((SELECT id from users where id = 1), (SELECT id from roles where id = 1));
 
 INSERT INTO user_role (user_id, role_id)
-values ((SELECT user_id from users where user_id = 2), (SELECT role_id from roles where role_id = 1));
+values ((SELECT id from users where id = 2), (SELECT id from roles where id = 1));
 
 INSERT INTO user_role (user_id, role_id)
-values ((SELECT user_id from users where user_id = 3), (SELECT role_id from users where role_id = 1));
+values ((SELECT id from users where id = 3), (SELECT id from users where id = 1));
 
 -- Address
 
@@ -54,13 +54,13 @@ VALUES ( 'Iowa city', 'Iowa', '4000W Street', '32557');
 
 INSERT INTO legal_entity (legal_entity_type, first_name, last_name, address_id, user_id, legal_entity_name,phone_number, company_name)
 VALUES ('company','', '',
-           (SELECT address_id from address where address_id = 1), (SELECT user_id from users where user_id = 1), 'Blue Sky Realty', '941-234-3423', 'Blue Sky Realty');
+           (SELECT id from address where id = 1), (SELECT id from users where id = 1), 'Blue Sky Realty', '941-234-3423', 'Blue Sky Realty');
 
 INSERT INTO legal_entity (legal_entity_type, first_name, last_name, address_id, user_id, legal_entity_name,phone_number, company_name)
-VALUES ( 'person', 'Noah', 'Johns', (SELECT address_id from address where address_id = 2), (SELECT user_id from users where user_id = 1) , 'Noah Johns', '441-234-9823', '');
+VALUES ( 'person', 'Noah', 'Johns', (SELECT id from address where id = 2), (SELECT id from users where id = 1) , 'Noah Johns', '441-234-9823', '');
 
 INSERT INTO legal_entity ( legal_entity_type, first_name, last_name, address_id, user_id, legal_entity_name,phone_number, company_name)
-VALUES ( 'company', '', '', (SELECT address_id from address where address_id = 3), (SELECT user_id from users where user_id = 2), 'Landmark Realty Group', '441-234-2345', 'Landmark Realty Group');
+VALUES ( 'company', '', '', (SELECT id from address where id = 3), (SELECT id from users where id = 2), 'Landmark Realty Group', '441-234-2345', 'Landmark Realty Group');
 
 -- Admin
 
