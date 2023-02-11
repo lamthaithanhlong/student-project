137e2fcd937daaccf464bd5638258d55c753bc74
Sanjaya <sanjaya.jayasooriya@gmail.com>
2023-02-10 21:30:13 -0600

    SP-11: Create an Admin user when starting the application

--- src/main/java/mscs/hms/config/AuthenticationHandler.java
+++ src/main/java/mscs/hms/config/AuthenticationHandler.java
@@ -3,18 +3,12 @@ package mscs.hms.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import mscs.hms.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collection;
 
 @Component
 public class AuthenticationHandler implements AuthenticationSuccessHandler {
